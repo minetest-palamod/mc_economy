fcad19c45c5c77d18ff12e8b0952d71e84a0482e
AFCMS <afcm.contact@gmail.com>
2021-03-15 19:15:16 +0100

    add basic /pay command
    for testing

--- commands.lua
+++ commands.lua
@@ -1,3 +1,23 @@
--- minetest.register_chatcommand("pay", {
-	
--- })
+local S = minetest.get_translator(minetest.get_current_modname())
+
+minetest.register_chatcommand("pay", {
+	params = "<playername> <amount>",
+	description = "Pay money to another player",
+	func = function(name, param)
+		if (param == "") then
+			return false, S("Error: No params specified.")
+		end
+		params = {}
+		for substring in param:gmatch("%S+") do
+		   table.insert(params, substring)
+		end
+		local playername, amount = string.match(param, "%S+")
+		if params[2] and tonumber(params[2]) <= mc_economy.get_player_balance(name) then
+			mc_economy.add_player_balance(params[1], tonumber(params[2]), true, name)
+			mc_economy.add_player_balance(name, -tonumber(params[2]), false, name)
+			return true, "Done."
+		else
+			return false, "Failed"
+		end
+	end,
+})
--- init.lua
+++ init.lua
@@ -2,9 +2,9 @@ local modpath = minetest.get_modpath(minetest.get_current_modname())
 
 mc_economy = {}
 
-if minetest.request_insecure_environment() then
-	dofile(modpath.."/database.lua") --TODO
-else
-	dofile(modpath.."/storage.lua")
-end
+--if minetest.request_insecure_environment() then
+--dofile(modpath.."/database.lua") --TODO
+--else
+dofile(modpath.."/storage.lua")
 dofile(modpath.."/items.lua")
+dofile(modpath.."/commands.lua")
