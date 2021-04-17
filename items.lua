d4ac7aab85689fdf20a649d8ff7423fc5ceafff1
AFCMS <afcm.contact@gmail.com>
2021-04-18 01:53:27 +0200

    change money stack max

--- items.lua
+++ items.lua
@@ -4,6 +4,7 @@ local S = minetest.get_translator(minetest.get_current_modname())
 minetest.register_craftitem("mc_economy:money", {
 	description = "Money",
 	inventory_image = "default_stone.png",
+	stack_max = 64,
 	on_place = function(itemstack, placer, pointed_thing)
 		if placer:is_player() then
 			mc_economy.add_player_balance(placer:get_player_name(), 1000*itemstack:get_count(), true)
