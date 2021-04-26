4b67e638857a7ec0e621c387e0222dd49f41f53a
AFCMS <afcm.contact@gmail.com>
2021-04-27 00:34:35 +0200

    fix error with translation files

--- items.lua
+++ items.lua
@@ -2,7 +2,7 @@ local is_creative_enabled = minetest.is_creative_enabled
 local S = minetest.get_translator(minetest.get_current_modname())
 
 minetest.register_craftitem("mc_economy:money", {
-	description = "Money",
+	description = S("Money"),
 	inventory_image = "default_stone.png",
 	stack_max = 64,
 	on_place = function(itemstack, placer, pointed_thing)
